be38ca3d8508973fbeef11a248b9ab14c9421a04
Ian Bailey <IanKulin@kulin.com.au>
2022-09-13 21:20:11 +0800

    Fresh empty project

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ian Bailey on 13/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
